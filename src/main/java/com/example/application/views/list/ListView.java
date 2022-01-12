c739c5090e9096f1ca849aebccfade833c6f10c5
chetan.gunti <chetan_gunti@nissan.mail.co.jp>
2022-01-12 16:07:54 +0900

    made changes

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,34 +1,102 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.entity.Apiinfo;
+import com.example.application.data.service.ApiService;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
 
-@PWA(name = "Flow CRM Tutorial", shortName = "Flow CRM Tutorial", enableInstallPrompt = false)
-@Theme(themeFolder = "flowcrmtutorial")
-@PageTitle("list")
-@Route(value = "")
+@PageTitle("API Data | Data Tool")
+@Route(value = "", layout = MainLayout.class)
 public class ListView extends VerticalLayout {
+    Grid<Apiinfo> grid=new Grid<>(Apiinfo.class);
+    TextField filterText= new TextField();
+    ApiForm form;
+    ApiService service;
 
-    public ListView() {
-        setSpacing(false);
+    public ListView(ApiService service) {
+       this.service=service;
+       addClassName("list-view");
+       setSizeFull();
+       configureGrid();
+       configureForm();
+       add(getToolbar(),getContent());
+       updateList();
+       closeEditor();
+    }
+    private void closeEditor(){
+        form.setApiinfo(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+    private void updateList(){
+        grid.setItems(service.findAllApiinfos(filterText.getValue()));
+    }
+    private Component getContent(){
+        VerticalLayout content= new VerticalLayout(grid,form);
+        content.setFlexGrow(2,grid);
+        content.setFlexGrow(1,form);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
+    }
+    private void configureForm(){
+        form = new ApiForm(service.findAllApitypes());
+        form.addListener(ApiForm.SaveEvent.class,this::saveApiinfo);
+        form.addListener(ApiForm.DeleteEvent.class,this::deleteApiinfo);
+        form.addListener(ApiForm.CloseEvent.class, e -> closeEditor());
+    }
+    private void saveApiinfo(ApiForm.SaveEvent event){
+        service.saveApiinfo(event.getApiinfo());
+        updateList();
+        closeEditor();
+    }
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+    private void deleteApiinfo(ApiForm.DeleteEvent event){
+        service.deleteApiinfo(event.getApiinfo());
+        updateList();
+        closeEditor();
+    }
+    private Component getToolbar(){
+        filterText.setPlaceholder("Type API Name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+        Button addButton=new Button("Add API Info");
+        addButton.addClickListener(e->addApiinfo());
+        HorizontalLayout toolbar= new HorizontalLayout(filterText,addButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+    private void addApiinfo(){
+        grid.asSingleSelect().clear();
+        editApiinfo(new Apiinfo());
+    }
+    public void configureGrid(){
+        grid.addClassName("configure-grid");
+        grid.setSizeFull();
+        grid.setColumns("apiName","purpose","request","response");
+        grid.addColumn(Apiinfo -> Apiinfo.getApitype().getApitypename()).setHeader("Api_Type");
+        grid.getColumns().forEach(col->col.setAutoWidth(true));
+        grid.asSingleSelect().addValueChangeListener(e -> editApiinfo(e.getValue()));
+    }
+    private void editApiinfo(Apiinfo apiinfo){
+        if(apiinfo==null){
+            closeEditor();
+        } else{
+            form.setApiinfo(apiinfo);
+            form.setVisible(true);
+            addClassName("editing");
+        }
     }
 
 }
